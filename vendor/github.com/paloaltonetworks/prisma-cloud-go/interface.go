9676e5f032368a4507d62e61b3f9544f884ae18f
KICKdesigns <jwang@KICKdesigns.com>
2021-06-10 18:37:59 -0700

    Remove unecessary vendor files and add in credentials json for example

--- vendor/github.com/paloaltonetworks/prisma-cloud-go/interface.go
+++
@@ -1,8 +0,0 @@
-package prismacloud
-
-type PrismaCloudClient interface {
-	Initialize(string) error
-	Authenticate() error
-	Communicate(string, []string, interface{}, interface{}, interface{}) ([]byte, error)
-	Log(string, string, ...interface{})
-}
