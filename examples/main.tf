95212d5db717fa4075e58d86c51e06d76cb4b87c
Wyatt Gill <wgill@paloaltonetworks.com>
2021-08-03 11:21:07 -0500

    Update examples

--- examples/main.tf
+++ examples/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     prismacloudcompute = {
       source  = "hashicorp/prismacloudcompute"
-      version = "~>1.0.0"
+      version = "~> 1.0.0"
     }
   }
 }
@@ -11,72 +11,71 @@ provider "prismacloudcompute" {
   json_config_file = "creds.json"
 }
 
-#resource "prismacloudcompute_collection" "example1" {
-#  name   = "example collection 1"
-#  color  = "#FF0000"
-#  appids = [ "app1" ]
-#  coderepos = [ "coderepo1", "prefix1*" ]
-#  images = [ "prefix2*", "prefix3*" ]
-#  labels = [ "env:development", "env:staging" ]
-#  namespaces = [ "hamilton" ]
-#}
+resource "prismacloudcompute_collection" "example1" {
+  name       = "example collection 1"
+  color      = "#FF0000"
+  appids     = ["app1"]
+  coderepos  = ["coderepo1", "prefix1*"]
+  images     = ["prefix2*", "prefix3*"]
+  labels     = ["env:development", "env:staging"]
+  namespaces = ["hamilton"]
+}
 
-#resource "prismacloudcompute_policiesruntimecontainer" "example2" {
-#    learningdisabled = true
-#    rules {
-#        name = "my-rule"
-#	collections {
-#		name = "All"
-#	}
-#        processes = {
-#            effect = "alert"
-#        }
-#        network = {
-#            effect = "alert"
-#        }
-#        dns = {
-#            effect = "alert"
-#        }
-#        filesystem = {
-#            effect = "alert"
-#        }
-#        wildfireanalysis = "alert"
-#    }
-#}
+resource "prismacloudcompute_policiesruntimecontainer" "example2" {
+  learningdisabled = false
+  rules {
+    name = "example container runtime rule"
+    collections {
+      name = "All"
+    }
+    wildfireanalysis = "alert"
+    processes = {
+      "effect" : "alert"
+    }
+    network = {
+      "effect" : "alert"
+    }
+    dns = {
+      "effect" : "disable"
+    }
+    filesystem = {
+      effect = "alert"
+    }
+  }
+}
 
 resource "prismacloudcompute_policiesvulnerabilityimages" "example3" {
-    policytype = "containerVulnerability"
-    rules {
-        name = "my-rule"
-	collections {
-		name = "All"
-	}
-        alertthreshold = {
-                disabled = false
-                value = 4
-        }
-        blockthreshold = {
-                enabled = false
-                value = 0
-        }
+  policytype = "containerVulnerability"
+  rules {
+    name = "example image vulnerability rule"
+    collections {
+      name = "All"
     }
+    alertthreshold = {
+      disabled = false
+      value    = 4
+    }
+    blockthreshold = {
+      enabled = false
+      value   = 0
+    }
+  }
 }
 
-resource "prismacloudcompute_policiescompliancecontainer" "example4" {
-    policytype = "containerCompliance"
-    rules {
-        name = "my-rule"
-        effect = "alert"
-	collections {
-		name = "All"
-	}
-        alertthreshold = {
-                disabled = false
-                value = 4
-        }
-        blockthreshold = {
-                enabled = false
-                value = 0
-        }
-   }
-}
+
+# resource "prismacloudcompute_policiescompliancecontainer" "example4" {
+#   policytype = "containerCompliance"
+#   rules {
+#     name   = "example container compliance rule"
+#     effect = "alert"
+#     collections {
+#       name = "All"
+#     }
+#     condition = {
+#       vulnerabilities = {
+#         id    = 531
+#         block = false
+#       }
+#     }
+#   }
+# }
