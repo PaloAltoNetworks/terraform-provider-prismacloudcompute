ad75a9b509ef9f30f4840e4b5bed6ed5e6b53faa
Jeremy Phillips <jeremy.phillips@synopsys.com>
2021-11-19 13:57:46 -0500

    Include example (which cares through to instructions) for using provider from tf registry

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    prismacloudcompute = {
+      source  = "PaloAltoNetworks/prismacloudcompute"
+      version = "0.1.0"
+    }
+  }
+}
+
 provider "prismacloudcompute" {
   config_file = "creds.json"
 }
