68f68b86690804438f17dc36e710f0eaab0f7549
Peter Nancarrow <37298202+pnancarrow@users.noreply.github.com>
2022-01-25 23:06:57 -0600

    Coderepo vulnerability policies (#1)
    * add vulnerabilities policies for coderepo & coderepo ci
    
    * fmt

--- internal/provider/common.go
+++ internal/provider/common.go
@@ -1,13 +1,15 @@
 package provider
 
 const (
-	policyTypeAdmission            = "admission"
-	policyTypeComplianceCiImage    = "ciImagesCompliance"
-	policyTypeComplianceContainer  = "containerCompliance"
-	policyTypeComplianceHost       = "hostCompliance"
-	policyTypeRuntimeContainer     = "containerRuntime"
-	policyTypeRuntimeHost          = "hostRuntime"
-	policyTypeVulnerabilityCiImage = "ciImagesVulnerability"
-	policyTypeVulnerabilityHost    = "hostVulnerability"
-	policyTypeVulnerabilityImage   = "containerVulnerability"
+	policyTypeAdmission               = "admission"
+	policyTypeComplianceCiImage       = "ciImagesCompliance"
+	policyTypeComplianceContainer     = "containerCompliance"
+	policyTypeComplianceHost          = "hostCompliance"
+	policyTypeRuntimeContainer        = "containerRuntime"
+	policyTypeRuntimeHost             = "hostRuntime"
+	policyTypeVulnerabilityCiCoderepo = "ciCodeRepoVulnerability"
+	policyTypeVulnerabilityCiImage    = "ciImagesVulnerability"
+	policyTypeVulnerabilityCoderepo   = "codeRepoVulnerability"
+	policyTypeVulnerabilityHost       = "hostVulnerability"
+	policyTypeVulnerabilityImage      = "containerVulnerability"
 )
