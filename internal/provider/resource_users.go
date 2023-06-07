b5cd710e8d464717ef1d106ad3f56b429fd5d68e
Max Boynton <mboynton@paloaltonetworks.com>
2023-06-07 16:51:39 -0500

    update user resource definition to use context-aware CRUD functions

--- internal/provider/resource_users.go
+++ internal/provider/resource_users.go
@@ -1,20 +1,21 @@
 package provider
 
 import (
-	"fmt"
+	"context"
 
 	"github.com/PaloAltoNetworks/terraform-provider-prismacloudcompute/internal/api"
 	"github.com/PaloAltoNetworks/terraform-provider-prismacloudcompute/internal/api/auth"
 	"github.com/PaloAltoNetworks/terraform-provider-prismacloudcompute/internal/convert"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceUsers() *schema.Resource {
 	return &schema.Resource{
-		Create: createUser,
-		Read:   readUser,
-		Update: updateUser,
-		Delete: deleteUser,
+		CreateContext: createUser,
+		ReadContext:   readUser,
+		UpdateContext: updateUser,
+		DeleteContext: deleteUser,
 
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -73,59 +74,66 @@ func resourceUsers() *schema.Resource {
 	}
 }
 
-func createUser(d *schema.ResourceData, meta interface{}) error {
+func createUser(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*api.Client)
 	parsedUser, err := convert.SchemaToUser(d)
 	if err != nil {
-		return fmt.Errorf("failed to create user '%+v': %s", parsedUser, err)
+		return diag.Errorf("failed to create user '%+v': %s", parsedUser, err)
 	}
 
 	if err := auth.CreateUser(*client, parsedUser); err != nil {
-		return fmt.Errorf("failed to create user '%+v': %s", parsedUser, err)
+		return diag.Errorf("failed to create user '%+v': %s", parsedUser, err)
 	}
 
 	d.SetId(parsedUser.Username)
-	return readUser(d, meta)
+	return readUser(ctx, d, meta)
 }
 
-func readUser(d *schema.ResourceData, meta interface{}) error {
+func readUser(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*api.Client)
+
+	var diags diag.Diagnostics
+
 	retrievedUser, err := auth.GetUser(*client, d.Id())
 	if err != nil {
-		return fmt.Errorf("failed to read user: %s", err)
+		return diag.Errorf("failed to read user: %s", err)
 	}
 
 	d.Set("authentication_type", retrievedUser.AuthType)
 	d.Set("password", retrievedUser.Password)
 	if err := d.Set("permissions", convert.UserPermissionsToSchema(retrievedUser.Permissions)); err != nil {
-		return fmt.Errorf("failed to read user: %s", err)
+		return diag.Errorf("failed to read user: %s", err)
 	}
 	d.Set("role", retrievedUser.Role)
 	d.Set("username", retrievedUser.Username)
 
-	return nil
+	return diags
 }
 
-func updateUser(d *schema.ResourceData, meta interface{}) error {
+func updateUser(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*api.Client)
 	parsedUser, err := convert.SchemaToUser(d)
 	if err != nil {
-		return fmt.Errorf("failed to update user: %s", err)
+		return diag.Errorf("failed to update user: %s", err)
 	}
 
 	if err := auth.UpdateUser(*client, parsedUser); err != nil {
-		return fmt.Errorf("failed to update user: %s", err)
+		return diag.Errorf("failed to update user: %s", err)
 	}
 
-	return readUser(d, meta)
+	return readUser(ctx, d, meta)
 }
 
-func deleteUser(d *schema.ResourceData, meta interface{}) error {
+func deleteUser(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	client := meta.(*api.Client)
+
+	var diags diag.Diagnostics
+
 	if err := auth.DeleteUser(*client, d.Id()); err != nil {
-		return fmt.Errorf("failed to delete user '%s': %s", d.Id(), err)
+		return diag.Errorf("failed to delete user '%s': %s", d.Id(), err)
 	}
 
 	d.SetId("")
-	return nil
+
+	return diags
 }
